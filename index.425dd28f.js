18a75ca09e18849d6365878c2e69e3552a7653b8
SvitlanaDy <110805508+SvitlanaDy@users.noreply.github.com>
2023-03-01 11:26:57 +0000

    Deploying to gh-pages from @ SvitlanaDy/js-team-project@7372fe0574f68de1b3f9dff026a938f4134d6ab2 🚀

---
+++ index.425dd28f.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("theme-switch");e.checked=!1,e.addEventListener("click",(function(){this.checked?(document.body.classList.remove("light"),document.body.classList.add("dark"),localStorage.setItem("theme","dark")):(document.body.classList.add("light"),document.body.classList.remove("dark"),localStorage.setItem("theme","light"))}));
+//# sourceMappingURL=index.425dd28f.js.map
